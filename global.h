2b603fcbca3a3ea1141ac23b608587fecad5be5d
Alan Gibson <alan.gibson@gmail.com>
2024-08-26 15:00:44 +0200

    Estop and float switches working

--- global.h
+++ global.h
@@ -12,8 +12,9 @@
 #define PIN_ARC_OK 26
 #define PIN_OHMIC_ENABLE 27
 #define PIN_OHMIC_PROBE 4
-
-#define PIN_ESTOP 22
+// #define PIN_ESTOP 22
+#define PIN_ESTOP 13
 #define PIN_CLOCK 18
 #define PIN_TORCH_BREAKAWAY 1
-#define PIN_TORCH_FLOAT 12
+// #define PIN_TORCH_FLOAT 12
+#define PIN_TORCH_FLOAT 19
