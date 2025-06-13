7aeff82d7e742ef691f3e1990c2400ea1e48a505
Alan Gibson <alan.gibson@gmail.com>
2025-06-13 16:36:19 +0200

    Changing pin numbers

--- global.h
+++ global.h
@@ -13,24 +13,20 @@
 
 // Define GPIO pin numbers
 //
-// pin 31 / TORCH_FIRE
-#define PIN_TORCH_ON 6
+// pin 38 / TORCH_FIRE
+#define PIN_TORCH_ON 20
 // pin 28 / ARC_FREQ
 #define PIN_ARC_FREQ 1
 // pin 37 / ARC_OK
 #define PIN_ARC_OK 26
-// pin 27 / IHS_ENABLE
-#define PIN_OHMIC_ENABLE 0
-// pin 29 / IHS_SENSE
-#define PIN_OHMIC_PROBE 5
-// pin 33 / ESTOP
-#define PIN_ESTOP 13
-// unused
-// #define PIN_CLOCK 18
-// unused
-// #define PIN_TORCH_BREAKAWAY 1
-// pin 35 / TORCH_FLOAT
-#define PIN_TORCH_FLOAT 19
+// pin 10 / IHS_ENABLE
+#define PIN_OHMIC_ENABLE 15
+// pin 8 / IHS_SENSE
+#define PIN_OHMIC_PROBE 14
+// pin 36 / ESTOP
+#define PIN_ESTOP 16
+// pin 32 / TORCH_FLOAT
+#define PIN_TORCH_FLOAT 12
 // pin 40 / TORCH_LASER
 #define PIN_TORCH_LASER 21
 
